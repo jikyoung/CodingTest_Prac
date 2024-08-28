d0cef36d0b091be6628eb228b906ec150ebdfe51
jikyoung <116933637+jikyoung@users.noreply.github.com>
2024-08-28 14:29:15 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
@@ -1,12 +1,6 @@
-import math
-
-def is_perfect_square(n):
-    sqrt_n = math.sqrt(n)  # 부동소수점 제곱근 계산
-    return sqrt_n.is_integer()  # 제곱근이 정수인지 확인
-
 def solution(n):
-    if is_perfect_square(n):
-        sqrt_n = int(math.sqrt(n))  # 정수 제곱근 계산
-        return (sqrt_n + 1) ** 2
+    sqrt = n ** (0.5)
+    if sqrt%1 == 0:
+        return (sqrt+1)**2
     else:
         return -1
